b6f3babd3e81d61c69b7179db63b250fc81e2e30
tjdgus903 <tjdgusgus123@gmail.com>
2024-04-19 07:52:43 +0900

    vue로 만드는 부동산 쇼핑몰

---
+++ src/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    <img :src="원룸들.image" class="room-img">
+    <h4 @click="send">{{ 원룸들.title }}</h4>
+    <p>{{ 원룸들.price }}원</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "Card",
+    props : {
+        원룸들 : Object,
+    },
+    methods : {
+        send(){
+            this.$emit('openModal', this.원룸들.id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="black-bg" v-if="모달창 == true">
+    <div class="white-bg">
+      <img :src="원룸들[누른거].image">
+      <h4>{{ 원룸들[누른거].title }}</h4>
+      <p>{{ 원룸들[누른거].content }}</p>
+      <input v-model.number="month">
+      <!--<input @input="month = $event.target.value">
+      <textarea v-model="month"></textarea>
+      <select v-model="month">
+        <option></option>
+        <option></option>
+      </select>-->
+      <p> {{ month }} 개월 선택함 : {{ 원룸들[누른거].price * month }} 원</p>
+      <button @click="closeModal">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Modal',
+    data() {
+      return {
+        month : 1,
+      }
+    },
+    props : {
+        원룸들 : Array,
+        누른거 : Number,
+        모달창 : Boolean,
+    },
+    methods : {
+      closeModal() {
+        this.$emit('closeModal')
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
